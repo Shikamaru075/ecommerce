d774122f5074c3a7b5d3f6c98c44c0f0eb8f5cd3
Ahmed Oueslati <ahmed.oueslati@Digital-Grenoble.local>
2022-07-28 15:35:20 +0200

    commencement fiche produit

---
+++ item.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Aspirator</title>
+</head>
+
+
+<body>
+
+$name="Aspirator";
+$price="500euros";
+url="aspirator.jpg";
+
+<h1>Aspirator</h1>
+
+
+
+
+
+
+
+
+
+</body>
+
+</html>
+
+
+<?php
